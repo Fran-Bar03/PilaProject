897957875ed6936306a3564ef6028648c760c0b2
Fran-Bar03 <papihachi_vlk@outlook.com>
2024-10-28 23:29:45 -0700

    Primer avance

---
+++ Pila.cs
@@ -0,0 +1,118 @@
+﻿using ProyectoPila;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPila
+{
+    internal class Pila
+    {
+        private int MAX;
+        private int tope = 0;
+        private Nodo inicio;
+        public Pila(int max)
+        {
+            MAX = max;
+            inicio = null;
+        }
+
+        public bool Empty()
+        {
+            if (inicio == null)
+                return true;
+            else
+                return false;
+        }
+
+        public bool Full()
+        {
+            if (tope == MAX)
+                return true;
+            else
+                return false;
+        }
+
+        public void PrintStack()
+        {
+
+            if (Empty()) 
+            {
+                Console.WriteLine("La pila esta vacia");
+
+            }
+
+          Nodo actual = inicio;
+
+            while (actual != null)
+            {
+                Console.WriteLine($"{actual.Valor}");
+                actual = actual.Sig;
+            }
+
+
+
+
+
+            //Imprime la pila
+        }
+
+        public bool Push(int num)
+        {
+            if (Full()) 
+            {
+                Console.WriteLine("La pila esta llena");
+                return false;
+                
+            }
+
+         Nodo nuevo = new Nodo(num);
+
+            if (Empty())
+            {
+                inicio = nuevo;
+            }
+
+            else
+            {
+                nuevo.Sig = inicio;
+                inicio = nuevo;
+            }
+
+            tope++;
+            return true;
+
+
+            //regresa true si la inserccion es exitosa
+            //regresa false si la píla esta llena y no se inserto
+        }
+
+        public int Pop()
+        {
+            if (Empty()) 
+            {
+                Console.WriteLine("No se puedo eliminar, lista vacia");
+                return -1;
+            }
+
+            int ValorE = inicio.Valor;
+
+            inicio = inicio.Sig;
+
+            tope--;
+
+            Console.WriteLine($"El numero sacado de la pila : {ValorE}");
+
+            return ValorE;
+            
+
+
+
+            //regresa el numero sacado de la pila
+            //si la pila esta vacia regresa -1
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila Mipila = null;
+           int op = 0;
+            do {
+            Console.WriteLine("|-----------------------------------|");
+            Console.WriteLine("|--------- PROYECTO PILAS ----------|");
+            Console.WriteLine("|-----------------------------------|");
+            Console.WriteLine("");
+            Console.WriteLine("1.Insertar tamaño");
+            Console.WriteLine("2.Push");
+            Console.WriteLine("3.Pop");
+            Console.WriteLine("4.Imprimir");
+            Console.WriteLine("5.Salir");
+            Console.WriteLine("");
+            Console.WriteLine("Seleccionar opcion =>");
+            op = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+
+
+
+                switch (op) 
+            {
+                case 1:
+                    Console.WriteLine("Dame el numero a insertar");
+                    int max = Convert.ToInt32(Console.ReadLine());
+
+                    if(max > 0) 
+                    {
+                        Mipila = new Pila(max);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Introduzca un tamaño de pila mayor a 0");
+                    }
+
+                    break;
+
+                case 2:
+
+                        if (Mipila == null)
+                        {
+                            Console.WriteLine("Introduzca un tamaño de pila primeramente");
+                        }
+                        else 
+                        { 
+                        Console.WriteLine("Ingresa el numero a insertar");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Mipila.Push(num);
+                        }
+                        break;
+
+                    case 3:
+
+                        if (Mipila == null)
+                        {
+                            Console.WriteLine("Introduzca un tamaño de pila primeramente");
+                        }
+
+                        else 
+                        { 
+
+                        int res =  Mipila.Pop();
+                        
+
+                        }
+
+                        break;
+
+                    case 4:
+
+                    Mipila.PrintStack();
+
+                    break;
+
+                    case 5:
+
+                    Console.WriteLine("Saliendo....");
+
+                    break;
+
+                    default:
+                    Console.WriteLine("Introduzca una opcion valida");
+                    break;
+
+                } 
+
+
+            } while (op != 5);
+
+
+        }
+    }
+}
